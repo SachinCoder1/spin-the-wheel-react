1a61af185e31c2ee5b34ea1639993b59c46b5b1f
SachinCoder1 <sachinhlo232@gmail.com>
2022-03-07 14:18:54 +0530

    optimized

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,42 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import { data } from './Data';
 
 function Wheel() {
     const [angle, setangle] = useState()
-
     let randomSeconds = Math.round(Math.random() * 10)
     let totalRotates = randomSeconds * 360;
 
-    const handleClick = () => {
-        let nthElement = totalRotates / 360;
-        setangle(nthElement * 45)
-        console.log(`The arrow is pointing to ${angle} degree in the circle`)
+    let nthElement = totalRotates / 360;
+    
+    const handleClick = async () => {
+        let finalAngle = nthElement * 45
+        setangle(finalAngle)
+        console.log(`The arrow is pointing to ${finalAngle} degree in the circle`)
     }
-
-
-
+    
 
 
 
     return (
-        <div> <button onClick={() => handleClick()} id="spin">Spin</button>
-            <span className="arrow">
-            </span>
+        <div>
+            <button onClick={handleClick} id="spin"> Spin </button>
+
+            <span className="arrow"> </span>
+
             <div style={{ transform: `rotate(${totalRotates}deg)` }} className="container">
-                <div className="one">1</div>
-                <div className="two">2</div>
-                <div className="three">3</div>
-                <div className="four">4</div>
-                <div className="five">5</div>
-                <div className="six">6</div>
-                <div className="seven">7</div>
-                <div className="eight">8</div>
+
+                {
+                    data?.map((element, index) => <div key={index} className={element.class}>{element.text}</div>)
+                }
+
             </div>
 
             <div>
+
                 <span className="anglerotate">
                     {angle && angle + " deg"}
                 </span>
-            </div></div>
+
+            </div>
+
+        </div>
     )
 }
 
